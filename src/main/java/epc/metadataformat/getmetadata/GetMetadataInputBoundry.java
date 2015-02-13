b42b86cc4b5a365a5c9009f6fd2fcfe822b0d764
olov <olov.mckie@ub.uu.se>
2015-02-13 09:02:44 +0100

    adding some documentation

--- src/main/java/epc/metadataformat/getmetadata/GetMetadataInputBoundry.java
+++ src/main/java/epc/metadataformat/getmetadata/GetMetadataInputBoundry.java
@@ -2,7 +2,22 @@ package epc.metadataformat.getmetadata;
 
 import epc.metadataformat.CoherentMetadata;
 
+/**
+ * GetMetadataInputBoundry is the boundry for accessing GetMetadata from other
+ * projects
+ * 
+ * @author <a href="mailto:olov.mckie@ub.uu.se">Olov McKie</a>
+ *
+ * @since
+ *
+ */
 public interface GetMetadataInputBoundry {
-
+	/**
+	 * getAllMetadata returns all metadata for the whole system, as a
+	 * CoherentMetadata populated with metadataFormat, Presentation, Collections
+	 * and Texts
+	 * 
+	 * @return A CoherentMetadata with all metadata for the entire system
+	 */
 	CoherentMetadata getAllMetadata();
 }
--- src/main/java/epc/metadataformat/storage/MetadataCache.java
+++ src/main/java/epc/metadataformat/storage/MetadataCache.java
@@ -4,7 +4,8 @@ import epc.metadataformat.CoherentMetadata;
 
 /**
  * MetadataLoader loads metadata from storage using an injected
- * MetadataStorageGateway
+ * MetadataStorageGateway. This cache automatically populates itself on startup
+ * using the MetadataStorageGateway.
  * 
  * @author <a href="mailto:olov.mckie@ub.uu.se">Olov McKie</a>
  *
@@ -15,14 +16,37 @@ public class MetadataCache {
 
 	private final CoherentMetadata coherentMetadata;
 
+	/**
+	 * Constructor, loads all metadata into this cache using the injected
+	 * MetadataStorageGateway
+	 * 
+	 * @param metadataStorageGateway
+	 */
 	public MetadataCache(MetadataStorageGateway metadataStorageGateway) {
 		coherentMetadata = metadataStorageGateway.loadAllMetadata();
 	}
 
+	/**
+	 * getTextTranslation returns the requested text using the requested
+	 * language
+	 * 
+	 * @param languageId
+	 *            A String with languageI such as "sv"
+	 * @param textId
+	 *            A String with the textId
+	 * @return A String with the requested translation
+	 */
 	public String getTextTranslation(String languageId, String textId) {
 		return coherentMetadata.getTextTranslation(languageId, textId);
 	}
 
+	/**
+	 * getAllMetadata returns all metadata for the whole system, as a
+	 * CoherentMetadata populated with metadataFormat, Presentation, Collections
+	 * and Texts
+	 * 
+	 * @return A CoherentMetadata with all metadata for the entire system
+	 */
 	public CoherentMetadata getAllMetadata() {
 		return coherentMetadata;
 	}
