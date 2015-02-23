049859f1ec7bbddb9954f34553ce406da3a0be20
olov <olov.mckie@ub.uu.se>
2015-02-23 11:19:49 +0100

    change name of RegExVariable to TextVariable

--- src/main/java/epc/metadataformat/TextVariable.java
+++ src/main/java/epc/metadataformat/TextVariable.java
@@ -1,7 +1,7 @@
 package epc.metadataformat;
 
 /**
- * RegExVariable is the class that handles metadata for a RegularExpression
+ * TextVariable is the class that handles metadata for a RegularExpression
  * variable
  * 
  * @author <a href="mailto:olov.mckie@ub.uu.se">Olov McKie</a>
@@ -9,11 +9,11 @@ package epc.metadataformat;
  * @since 0.1
  *
  */
-public class RegExVariable extends MetadataElement {
+public class TextVariable extends MetadataElement {
 
 	private final String regularExpression;
 
-	public RegExVariable(String id, String dataId, String textId,
+	public TextVariable(String id, String dataId, String textId,
 			String deffTextId, String regularExpression) {
 		super(id, dataId, textId, deffTextId);
 		this.regularExpression = regularExpression;
--- src/test/java/epc/metadataformat/TextVariableTest.java
+++ src/test/java/epc/metadataformat/TextVariableTest.java
@@ -4,26 +4,26 @@ import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
-public class RegExVariableTest {
+public class TextVariableTest {
 	@Test
 	public void testRegExVariableInit() {
 		String regularExpression = "((^(([0-1][0-9])|([2][0-3])):[0-5][0-9]$|^$){1}";
-		RegExVariable regExVar = new RegExVariable("id", "dataId", "textId",
+		TextVariable textVar = new TextVariable("id", "dataId", "textId",
 				"deffTextId", regularExpression);
 
-		assertEquals(regExVar.getId(), "id",
+		assertEquals(textVar.getId(), "id",
 				"Id should have the value set in the constructor");
 		
-		assertEquals(regExVar.getDataId(), "dataId",
+		assertEquals(textVar.getDataId(), "dataId",
 				"DataId should have the value set in the constructor");
 		
-		assertEquals(regExVar.getTextId(), "textId",
+		assertEquals(textVar.getTextId(), "textId",
 				"TextId should have the value set in the constructor");
 		
-		assertEquals(regExVar.getDeffTextId(), "deffTextId",
+		assertEquals(textVar.getDeffTextId(), "deffTextId",
 				"DeffTextId should have the value set in the constructor");
 		
-		assertEquals(regExVar.getRegularExpression(), regularExpression,
+		assertEquals(textVar.getRegularExpression(), regularExpression,
 				"RegularExpression should have the value set in the constructor");
 	}
 }
