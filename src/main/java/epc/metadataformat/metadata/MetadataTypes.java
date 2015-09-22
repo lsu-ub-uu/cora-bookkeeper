6a234da04ae2063cfbb8c56841c93eba0f6203b5
support <support@ub-epc-d-adm-999>
2015-09-22 15:23:08 +0200

    EPC-82 Work on abstract recordTypes, test coverage and sonar complaint

--- src/main/java/epc/metadataformat/metadata/MetadataTypes.java
+++ src/main/java/epc/metadataformat/metadata/MetadataTypes.java
@@ -1,13 +1,15 @@
 package epc.metadataformat.metadata;
 
 public enum MetadataTypes {
-
-	GROUP("metadataGroup"), TEXTVARIABLE("metadataTextVariable"), COLLECTIONVARIABLE(
-			"metadataCollectionVariable"), ITEMCOLLECTION("metadataItemCollection"), COLLECTIONITEM(
-					"metadataCollectionItem");
+	GROUP("metadataGroup"), 
+	TEXTVARIABLE("metadataTextVariable"), 
+	COLLECTIONVARIABLE("metadataCollectionVariable"), 
+	ITEMCOLLECTION("metadataItemCollection"), 
+	COLLECTIONITEM("metadataCollectionItem");
+	
 	private MetadataTypes(String type) {
 		this.type = type;
 	}
 
-	public String type;
+	public final String type;
 }
