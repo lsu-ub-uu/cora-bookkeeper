f60a618537e239cb89a3ca75c427326fb82fe9b5
support <support@ub-epc-d-adm-999>
2015-10-07 16:12:22 +0200

    CORA-85 added new metadata type dataDataLink

--- src/main/java/se/uu/ub/cora/metadataformat/metadata/MetadataTypes.java
+++ src/main/java/se/uu/ub/cora/metadataformat/metadata/MetadataTypes.java
@@ -1,12 +1,10 @@
 package se.uu.ub.cora.metadataformat.metadata;
 
 public enum MetadataTypes {
-	GROUP("metadataGroup"), 
-	TEXTVARIABLE("metadataTextVariable"), 
-	COLLECTIONVARIABLE("metadataCollectionVariable"), 
-	ITEMCOLLECTION("metadataItemCollection"), 
-	COLLECTIONITEM("metadataCollectionItem");
-	
+	GROUP("metadataGroup"), TEXTVARIABLE("metadataTextVariable"), COLLECTIONVARIABLE(
+			"metadataCollectionVariable"), ITEMCOLLECTION("metadataItemCollection"), COLLECTIONITEM(
+					"metadataCollectionItem"), DATATODATALINK("metadataDataToDataLink");
+
 	private MetadataTypes(String type) {
 		this.type = type;
 	}
