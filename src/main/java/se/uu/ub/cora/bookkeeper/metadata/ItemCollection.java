bf4b8f04e0d53afd911c1480e401f50bf5966420
Uwe Klosa <uwe.klosa@ub.uu.se>
2015-11-02 11:37:10 +0100

    changed version to 0.4-SNAPSHOT and added missing license headers

--- src/main/java/se/uu/ub/cora/bookkeeper/metadata/ItemCollection.java
+++ src/main/java/se/uu/ub/cora/bookkeeper/metadata/ItemCollection.java
@@ -16,6 +16,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package se.uu.ub.cora.bookkeeper.metadata;
 
 import java.util.ArrayList;
