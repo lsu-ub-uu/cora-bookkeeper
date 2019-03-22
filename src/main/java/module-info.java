2fc4a030516eb63a8c1d7cb29434163f147d7a17
olovm <olov.mckie@ub.uu.se>
2019-03-22 14:24:59 +0000

    updating to 1.0 for module change

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module se.uu.ub.cora.bookkeeper {
+	requires se.uu.ub.cora.json;
+
+	// exports se.uu.ub.cora.json.builder;
+	// exports se.uu.ub.cora.json.builder.org;
+	// exports se.uu.ub.cora.json.parser;
+	// exports se.uu.ub.cora.json.parser.org;
+}
