db38fbf9884e9025ab1fb7834177c66387442856
olovm <olov.mckie@ub.uu.se>
2019-06-11 06:11:52 +0000

    updated license with year

--- src/main/java/se/uu/ub/cora/bookkeeper/linkcollector/DataRecordLinkCollectorImp.java
+++ src/main/java/se/uu/ub/cora/bookkeeper/linkcollector/DataRecordLinkCollectorImp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, 2017 Uppsala University Library
+ * Copyright 2015, 2017, 2019 Uppsala University Library
  *
  * This file is part of Cora.
  *
--- src/main/java/se/uu/ub/cora/bookkeeper/termcollector/DataGroupTermCollectorImp.java
+++ src/main/java/se/uu/ub/cora/bookkeeper/termcollector/DataGroupTermCollectorImp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, 2018 Uppsala University Library
+ * Copyright 2017, 2018, 2019 Uppsala University Library
  *
  * This file is part of Cora.
  *
