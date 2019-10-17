61db39a9d3b117b6cd540e2e537a62ce54452a70
madde <madde@5a8feae0c08c>
2019-10-17 19:40:14 +0000

    Added test for CollectedDataCreator

--- src/test/java/se/uu/ub/cora/bookkeeper/termcollector/CollectedDataCreatorTest.java
+++ src/test/java/se/uu/ub/cora/bookkeeper/termcollector/CollectedDataCreatorTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2019 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.uu.ub.cora.bookkeeper.termcollector;
 
 import static org.testng.Assert.assertEquals;
@@ -7,6 +25,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import se.uu.ub.cora.data.DataAtomic;
@@ -14,13 +33,22 @@ import se.uu.ub.cora.data.DataGroup;
 
 public class CollectedDataCreatorTest {
 
+	private CollectedDataCreatorImp dataCreator;
+	private Map<String, List<DataGroup>> collectedTerms;
+	private List<DataGroup> indexDataGroups;
+
+	@BeforeMethod
+	public void setUp() {
+		dataCreator = new CollectedDataCreatorImp();
+		collectedTerms = new HashMap<>();
+		indexDataGroups = new ArrayList<>();
+	}
+
 	@Test
 	public void testCreateCollectedDataNoCollectTerms() {
-		CollectedDataCreatorImp dataCreator = new CollectedDataCreatorImp();
 		DataGroup dataGroup = createBasicDataGroup();
-		Map<String, List<DataGroup>> collectedTerms = new HashMap<>();
 		DataGroup result = dataCreator
-				.createCollectedDataFromCollectedTermsAndRecord(collectedTerms, dataGroup);
+				.createCollectedDataFromCollectedTermsAndRecord(new HashMap<>(), dataGroup);
 
 		assertEquals(result.getNameInData(), "collectedData");
 		assertEquals(result.getChildren().size(), 2);
@@ -29,18 +57,12 @@ public class CollectedDataCreatorTest {
 	}
 
 	@Test
-	public void testCreateCollectedDataOneCollectTermAtomicValue() {
-		CollectedDataCreatorImp dataCreator = new CollectedDataCreatorImp();
+	public void testCreateCollectedDataOneCollectIndexTerm() {
 		DataGroup dataGroup = createBasicDataGroup();
-		dataGroup.addChild(DataAtomic.withNameInDataAndValue("bookTitle", "Some title"));
 
-		Map<String, List<DataGroup>> collectedTerms = new HashMap<>();
-		DataGroup dataTerm = DataGroup.withNameInData("collectedDataTerm");
-		dataTerm.addChild(DataAtomic.withNameInDataAndValue("collectTermId", "titleIndexTerm"));
-		dataTerm.addChild(DataAtomic.withNameInDataAndValue("collectTermValue", "Some title"));
-		List<DataGroup> dataGroups = new ArrayList<>();
-		dataGroups.add(dataTerm);
-		collectedTerms.put("index", dataGroups);
+		DataGroup dataTerm = createCollectedDataTerm("Some title", "titleIndexTerm");
+		indexDataGroups.add(dataTerm);
+		collectedTerms.put("index", indexDataGroups);
 
 		DataGroup result = dataCreator
 				.createCollectedDataFromCollectedTermsAndRecord(collectedTerms, dataGroup);
@@ -49,13 +71,73 @@ public class CollectedDataCreatorTest {
 		assertEquals(result.getChildren().size(), 3);
 		assertEquals(result.getFirstAtomicValueWithNameInData("type"), "book");
 		assertEquals(result.getFirstAtomicValueWithNameInData("id"), "book:111");
+
 		DataGroup indexDataGroup = result.getFirstGroupWithNameInData("index");
-		DataGroup collectedDataTerm = indexDataGroup
-				.getFirstGroupWithNameInData("collectedDataTerm");
-		assertEquals(collectedDataTerm.getFirstAtomicValueWithNameInData("collectTermId"),
-				"titleIndexTerm");
-		assertEquals(collectedDataTerm.getFirstAtomicValueWithNameInData("collectTermValue"),
-				"Some title");
+		assertCollectedTermInReturnedGroupIsSameAsSentIn(indexDataGroup, 0, "index");
+	}
+
+	@Test
+	public void testCreateCollectedDataTwoCollectTerms() {
+		DataGroup dataGroup = createBasicDataGroup();
+
+		DataGroup dataTerm = createCollectedDataTerm("Some title", "titleIndexTerm");
+		indexDataGroups.add(dataTerm);
+		DataGroup dataTerm2 = createCollectedDataTerm("Some title", "titleSecondIndexTerm");
+		indexDataGroups.add(dataTerm2);
+		collectedTerms.put("index", indexDataGroups);
+
+		DataGroup result = dataCreator
+				.createCollectedDataFromCollectedTermsAndRecord(collectedTerms, dataGroup);
+
+		assertEquals(result.getNameInData(), "collectedData");
+		assertEquals(result.getChildren().size(), 3);
+		assertEquals(result.getFirstAtomicValueWithNameInData("type"), "book");
+		assertEquals(result.getFirstAtomicValueWithNameInData("id"), "book:111");
+
+		DataGroup indexDataGroup = result.getFirstGroupWithNameInData("index");
+		assertCollectedTermInReturnedGroupIsSameAsSentIn(indexDataGroup, 0, "index");
+		assertCollectedTermInReturnedGroupIsSameAsSentIn(indexDataGroup, 1, "index");
+
+	}
+
+	private void assertCollectedTermInReturnedGroupIsSameAsSentIn(DataGroup indexDataGroup,
+			int index, String key) {
+		DataGroup collectedDataTerm = indexDataGroup.getAllGroupsWithNameInData("collectedDataTerm")
+				.get(index);
+		assertEquals(collectedDataTerm, collectedTerms.get(key).get(index));
+		assertEquals(collectedDataTerm.getRepeatId(), String.valueOf(index));
+	}
+
+	@Test
+	public void testCreateCollectedDataTwoDifferentTypesOfCollectTerms() {
+		DataGroup dataGroup = createBasicDataGroup();
+
+		DataGroup indexDataTerm = createCollectedDataTerm("Some title", "titleIndexTerm");
+		indexDataGroups.add(indexDataTerm);
+		DataGroup indexDataTerm2 = createCollectedDataTerm("Some sub title", "subTitleIndexTerm");
+		indexDataGroups.add(indexDataTerm2);
+		collectedTerms.put("index", indexDataGroups);
+
+		List<DataGroup> storageDataGroups = new ArrayList<>();
+		DataGroup storageDataTerm = createCollectedDataTerm("Some name", "nameIndexTerm");
+		storageDataGroups.add(storageDataTerm);
+		collectedTerms.put("storage", storageDataGroups);
+
+		DataGroup result = dataCreator
+				.createCollectedDataFromCollectedTermsAndRecord(collectedTerms, dataGroup);
+
+		assertEquals(result.getNameInData(), "collectedData");
+		assertEquals(result.getChildren().size(), 4);
+		assertEquals(result.getFirstAtomicValueWithNameInData("type"), "book");
+		assertEquals(result.getFirstAtomicValueWithNameInData("id"), "book:111");
+
+		DataGroup indexDataGroup = result.getFirstGroupWithNameInData("index");
+		assertCollectedTermInReturnedGroupIsSameAsSentIn(indexDataGroup, 0, "index");
+		assertCollectedTermInReturnedGroupIsSameAsSentIn(indexDataGroup, 1, "index");
+
+		DataGroup storageDataGroup = result.getFirstGroupWithNameInData("storage");
+		assertCollectedTermInReturnedGroupIsSameAsSentIn(storageDataGroup, 0, "storage");
+
 	}
 
 	private DataGroup createBasicDataGroup() {
@@ -76,6 +158,17 @@ public class CollectedDataCreatorTest {
 				"system", dataDivider);
 		recordInfo.addChild(dataDividerGroup);
 		return recordInfo;
+	}
+
+	private DataGroup createCollectedDataTerm(String collectTermValue, String collectTermId) {
+		DataGroup dataTerm = DataGroup.withNameInData("collectedDataTerm");
+		dataTerm.addChild(DataAtomic.withNameInDataAndValue("collectTermId", collectTermId));
+		dataTerm.addChild(DataAtomic.withNameInDataAndValue("collectTermValue", collectTermValue));
+		DataGroup extraData = DataGroup.withNameInData("extraData");
+		extraData.addChild(DataAtomic.withNameInDataAndValue("indexType", "indexTypeString"));
+		dataTerm.addChild(extraData);
+		dataTerm.addAttributeByIdWithValue("type", "index");
+		return dataTerm;
 	}
 
 }
