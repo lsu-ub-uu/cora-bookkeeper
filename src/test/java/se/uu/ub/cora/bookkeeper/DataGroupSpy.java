2f34dcfa396394b79d6cb1b8d924cd53dd30615a
maddekenn <madeleine.kennback@ub.uu.se>
2019-11-25 09:12:33 +0000

    New versions of dependencies

--- src/test/java/se/uu/ub/cora/bookkeeper/DataGroupSpy.java
+++ src/test/java/se/uu/ub/cora/bookkeeper/DataGroupSpy.java
@@ -1,11 +1,31 @@
+/*
+ * Copyright 2019 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.uu.ub.cora.bookkeeper;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import se.uu.ub.cora.data.DataAtomic;
+import se.uu.ub.cora.data.DataAttribute;
 import se.uu.ub.cora.data.DataElement;
 import se.uu.ub.cora.data.DataGroup;
 
@@ -38,7 +58,6 @@ public class DataGroupSpy implements DataGroup {
 	@Override
 	public String getFirstAtomicValueWithNameInData(String nameInData) {
 		return atomicValues.get(nameInData);
-		// return "someAtomicValueFromSpyFor" + nameInData;
 	}
 
 	@Override
@@ -98,14 +117,6 @@ public class DataGroupSpy implements DataGroup {
 		return attributes;
 	}
 
-	// @Override
-	// public DataElement getFirstChildWithNameInData(String nameInData) {
-	// DataGroupSpy dataGroupSpy = new DataGroupSpy(nameInData);
-	// if ("refCollection".contentEquals(nameInData)) {
-	// dataGroupSpy.addChild(new DataAtomicSpy("linkedRecordId", "someSpyLinkedRecordId"));
-	// }
-	// return dataGroupSpy;
-	// }
 	@Override
 	public DataElement getFirstChildWithNameInData(String nameInData) {
 		if (dataGroups.containsKey(nameInData)) {
@@ -161,7 +172,14 @@ public class DataGroupSpy implements DataGroup {
 	@Override
 	public void removeFirstChildWithNameInData(String childNameInData) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public Collection<DataGroup> getAllGroupsWithNameInDataAndAttributes(String childNameInData,
+			DataAttribute... childAttributes) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
