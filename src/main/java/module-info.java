96aa959f635b5af7120ac0c1fd7864620cb25eed
olov <olov@2abf716187cd>
2020-03-27 15:51:54 +0000

    javadoc updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,11 @@
+/**
+ * The bookkeeper module provides functionality needed to validate and process data based, on
+ * information from metadata, in a Cora based system.
+ * <p>
+ * The name bookkeeper is a reference to bookkeepers who keeps the finances in order. Every piece of
+ * data in a Cora based system is governed by the systems metadata and keeping that in order and
+ * managing the data records from it, is of utmost importance.
+ */
 module se.uu.ub.cora.bookkeeper {
 	requires transitive se.uu.ub.cora.data;
 	requires transitive se.uu.ub.cora.storage;
