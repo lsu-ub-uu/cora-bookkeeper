2b0ca294fa3736fe048d1030799e8ae3f395a0d0
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-03-04 10:03:44 +0000

    Refactor link collector classes and tests, remove deprecated classes
    - Deleted DataGroupRecordLinkCollector and related test
    - Updated DataRecordLinkCollectorImp with link collection logic
    - Updated DataRecordLinkCollectorTest with new metadata creator and
    tests
    - Removed MetadataStorageStub and cleaned up related dependencies

--- src/main/java/se/uu/ub/cora/bookkeeper/linkcollector/DataRecordLinkCollectorImp.java
+++ src/main/java/se/uu/ub/cora/bookkeeper/linkcollector/DataRecordLinkCollectorImp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, 2017, 2019 Uppsala University Library
+ * Copyright 2015, 2017, 2019, 2025 Uppsala University Library
  *
  * This file is part of Cora.
  *
@@ -19,32 +19,128 @@
 
 package se.uu.ub.cora.bookkeeper.linkcollector;
 
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
+import se.uu.ub.cora.bookkeeper.metadata.MetadataChildReference;
+import se.uu.ub.cora.bookkeeper.metadata.MetadataElement;
+import se.uu.ub.cora.bookkeeper.metadata.MetadataGroup;
 import se.uu.ub.cora.bookkeeper.metadata.MetadataHolder;
+import se.uu.ub.cora.bookkeeper.metadata.MetadataHolderProvider;
+import se.uu.ub.cora.bookkeeper.metadata.RecordLink;
+import se.uu.ub.cora.bookkeeper.validator.MetadataMatchData;
+import se.uu.ub.cora.bookkeeper.validator.MetadataMatchDataImp;
+import se.uu.ub.cora.bookkeeper.validator.ValidationAnswer;
+import se.uu.ub.cora.data.DataChild;
 import se.uu.ub.cora.data.DataGroup;
+import se.uu.ub.cora.data.DataParent;
 import se.uu.ub.cora.data.collected.Link;
 
 public class DataRecordLinkCollectorImp implements DataRecordLinkCollector {
-
+	private static final String LINKED_RECORD_TYPE = "linkedRecordType";
+	private static final String LINKED_RECORD_ID = "linkedRecordId";
 	private MetadataHolder metadataHolder;
+	private Set<Link> linkSet;
 
 	@Override
 	public Set<Link> collectLinks(String metadataId, DataGroup dataGroup) {
 		getMetadataFromStorage();
-		DataGroupRecordLinkCollector collector = new DataGroupRecordLinkCollector(metadataHolder);
-		return collectLinksAndAddToDataGroup(metadataId, dataGroup, collector);
-	}
-
-	private Set<Link> collectLinksAndAddToDataGroup(String metadataId, DataGroup dataGroup,
-			DataGroupRecordLinkCollector collector) {
-		return collector.collectLinks(metadataId, dataGroup);
+		linkSet = new LinkedHashSet<>();
+		collectLinksForCurrentLevel(metadataId, dataGroup);
+		return linkSet;
 	}
 
 	private void getMetadataFromStorage() {
-		metadataHolder = new MetadataHolderPopulatorImp()
-				.createAndPopulateMetadataHolderFromMetadataStorage();
-		// metadataHolder =MetadataHolderProvider.getHolder();
+		metadataHolder = MetadataHolderProvider.getHolder();
 	}
 
+	public void collectLinksForCurrentLevel(String metadataGroupId, DataGroup dataGroup) {
+		List<MetadataChildReference> metadataChildReferences = getMetadataGroupChildReferences(
+				metadataGroupId);
+		collectLinksFromDataGroupUsingMetadataChildren(metadataChildReferences, dataGroup);
+	}
+
+	private List<MetadataChildReference> getMetadataGroupChildReferences(String metadataGroupId) {
+		MetadataGroup metadataGroup = (MetadataGroup) metadataHolder
+				.getMetadataElement(metadataGroupId);
+		return metadataGroup.getChildReferences();
+	}
+
+	private void collectLinksFromDataGroupUsingMetadataChildren(
+			List<MetadataChildReference> metadataChildReferences, DataParent dataGroup) {
+		for (MetadataChildReference metadataChildReference : metadataChildReferences) {
+			collectLinksFromDataGroupUsingMetadataChild(metadataChildReference, dataGroup);
+		}
+	}
+
+	private void collectLinksFromDataGroupUsingMetadataChild(
+			MetadataChildReference metadataChildReference, DataParent dataGroup) {
+		String referenceId = metadataChildReference.getLinkedRecordId();
+
+		MetadataElement childMetadataElement = metadataHolder.getMetadataElement(referenceId);
+		if (metadataElementConcernsLinks(childMetadataElement)) {
+			collectLinksFromDataGroupChildren(childMetadataElement, dataGroup);
+		}
+	}
+
+	private boolean metadataElementConcernsLinks(MetadataElement childMetadataElement) {
+		return isRecordLink(childMetadataElement) || isMetadataGroup(childMetadataElement);
+	}
+
+	private boolean isRecordLink(MetadataElement childMetadataElement) {
+		return childMetadataElement instanceof RecordLink;
+	}
+
+	private boolean isMetadataGroup(MetadataElement childMetadataElement) {
+		return childMetadataElement instanceof MetadataGroup;
+	}
+
+	private void collectLinksFromDataGroupChildren(MetadataElement childMetadataElement,
+			DataParent dataGroup) {
+		for (DataChild childDataElement : dataGroup.getChildren()) {
+			collectLinksFromDataGroupChild(childMetadataElement, childDataElement);
+		}
+	}
+
+	private void collectLinksFromDataGroupChild(MetadataElement childMetadataElement,
+			DataChild childDataElement) {
+		if (childMetadataSpecifiesChildData(childMetadataElement, childDataElement)) {
+			createLinkOrParseChildGroup(childMetadataElement, childDataElement);
+		}
+	}
+
+	private boolean childMetadataSpecifiesChildData(MetadataElement childMetadataElement,
+			DataChild dataElement) {
+		MetadataMatchData metadataMatchData = MetadataMatchDataImp
+				.withMetadataHolder(metadataHolder);
+		ValidationAnswer validationAnswer = metadataMatchData
+				.metadataSpecifiesData(childMetadataElement, dataElement);
+		return validationAnswer.dataIsValid();
+	}
+
+	private void createLinkOrParseChildGroup(MetadataElement childMetadataElement,
+			DataChild childDataElement) {
+
+		if (isRecordLink(childMetadataElement)) {
+			createRecordToRecordLink((DataGroup) childDataElement);
+		} else {
+			collectLinksFromSubGroup(childMetadataElement, (DataGroup) childDataElement);
+		}
+	}
+
+	private void createRecordToRecordLink(DataGroup dataElement) {
+		linkSet.add(createToPart(dataElement));
+	}
+
+	private Link createToPart(DataGroup dataElement) {
+		String type = dataElement.getFirstAtomicValueWithNameInData(LINKED_RECORD_TYPE);
+		String id = dataElement.getFirstAtomicValueWithNameInData(LINKED_RECORD_ID);
+		return new Link(type, id);
+	}
+
+	private void collectLinksFromSubGroup(MetadataElement childMetadataElement,
+			DataGroup subGroup) {
+		collectLinksForCurrentLevel(childMetadataElement.getId(), subGroup);
+	}
 }
